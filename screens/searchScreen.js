f2092fa4bc5517e5237e9d5e713034bedee6c32b
nsinturi07 <65034759+nsinturi07@users.noreply.github.com>
2021-02-04 18:53:53 -0600

    Update searchScreen.js

--- screens/searchScreen.js
+++ screens/searchScreen.js
@@ -1,110 +1,196 @@
-import React from 'react';
-import { View, Text, FlatList, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import * as React from 'react';
+import { Text, View, FlatList, TouchableOpacity, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { Header, SearchBar } from 'react-native-elements';
 import db from '../config';
 
-export default class ReadStoryScreen extends React.Component{
-  constructor(props) {
-    super(props);
+export default class ReadStoryScreen extends React.Component {
 
-    this.state={
-      book: [],
-      search: '',
-      lastVisibleStory: null,
-    }
-  }
-
-  componentDidMount = async () => {
-    const allStories = await db.collection("story").limit(10).get();
-
-    allStories.docs.map((doc) => {
-      this.setState({
-        book: [...this.state.book, doc.data()],
-        lastVisibleStory: doc
-      });
-    });
-  }
-
-  fetchMoreStories = async (text) => {
-    const story = await db.collection("story").where('storyName', '==', text).startAfter(this.state.lastVisibleStory).limit(10).get();
-    story.docs.map((doc) => {
-      this.setState({
-        book: [...this.state.book, doc.data()],
-        lastVisibleStory: doc
-      });
-    });
-  }
-
-  searchFilterFunction = async (text) => {
-    const story = await db.collection("story").where('storyName', '==', text).get();
-    story.docs.map((doc) => {
-      this.setState({
-        book: [...this.state.book, doc.data()],
-        lastVisibleStory: doc
-      });
-    });
-  }
-
-  render(){
-    return (
-      <View style={styles.container}>
-        <View style={styles.searchBar}>
-          <TextInput
-            style={styles.bar}
-            placeholder="Enter book name"
-            onChangeText={(text) => {this.setState({search: text})}}
-          />
-
-          <TouchableOpacity 
-            style={styles.searchButton}
-            onPress={() => {this.searchFilterFunction(this.state.search)}}  
-          >
-            <Text>Search</Text>
-          </TouchableOpacity>
-        </View>
-
-          <FlatList 
-            data={this.state.book}
-            renderItem={({item}) => (
-              <View style={{borderBottomWidth: 2, marginTop: 20, marginBottom: 20}}>
-                <Text>{"Story Name: " + item.storyName}</Text>
-                <Text>{"Story Author: " + item.storyAuthor}</Text>
-              </View>
-            )}
-            keyExtractor={(item, index) => index.toString()}
-            onEndReached={this.fetchMoreStories}
-            onEndReachedThreshold={0.7}
-          />
-      </View>
-    );
-  }
+constructor () {
+super();
+this.state = {
+allEntries : [],
+lastVisibleEntry : null,
+search:'',
+searchWhere:'',
+isSearchPressed:false,
+}
 }
 
+componentDidMount = async()=> {
+
+const entries = await db.collection('stories').limit(10).get();
+entries.docs.map(
+(item)=>{
+this.setState({
+allEntries:[...this.state.allEntries,item.data()],
+lastVisibleEntry:item,
+})
+}
+)
+//console.log(this.state.allEntries)
+}
+
+//retrieveEntries = async ()=> {
+//  //console.log('Entered retrieveEntries')
+//  const entries = await db.collection('stories').liimit(10).get();
+//  entries.docs.map(
+//    (item)=>{
+//      this.setState({
+//        allEntries:[...this.state.allEntries,item.data()],
+//        lastVisibleEntry:item,
+//      })
+//    }
+//  )
+//  console.log('Exited RetrieveEntries')
+//}
+
+searchByFilter = async (arg1)=> {
+const txt = this.state.search;
+const entries = await db.collection('stories').where(arg1,'==',txt).limit(10).get()
+entries.docs.map(
+(item)=>{
+console.log(item.data())
+this.setState({
+allEntries:[...this.state.allEntries,item.data()],
+lastVisibleEntry:item,
+})
+}
+)
+}
+
+fetchMoreEntries = async ()=> {
+//console.log('Entered FetchMoreEntries')
+const searchPress = this.state.isSearchPressed;
+const searchWhere = this.state.searchWhere;
+const txt = this.state.search;
+if (!searchPress){
+const entries = await db.collection('stories').startAfter(this.state.lastVisibleEntry).limit(10).get()
+entries.docs.map(
+(item)=>{
+this.setState({
+allEntries:[...this.state.allEntries,item.data()],
+lastVisibleEntry:item,
+})
+}
+)
+}
+if (searchPress){
+const entries = await db.collection('stories').startAfter(this.state.lastVisibleEntry).where(searchWhere,'==',txt).limit(10).get()
+entries.docs.map(
+(item)=>{
+this.setState({
+allEntries:[...this.state.allEntries,item.data()],
+lastVisibleEntry:item,
+})
+}
+)
+}
+//console.log('Exited fetchMoreEntries')
+}
+
+render(){
+//console.log('inside Render')
+return(
+<KeyboardAvoidingView style = {styles.container}>
+<Header
+backgroundColor={'pink'}
+centerComponent={{
+text: 'Bedtime Stories',
+style: { color: '#fff', fontSize: 20 },
+}}
+/>
+<SearchBar
+placeholder="Search....."
+onChangeText={(search)=>{
+this.setState({
+search:search,
+})
+}}
+value = {this.state.search}
+>
+</SearchBar>
+<View><TouchableOpacity onPress = {()=>{
+this.setState({
+searchWhere:'storyAuthor',
+allEntries:[],
+lastVisibleEntry:null,
+isSearchPressed:true,
+})
+this.searchByFilter('storyAuthor')}
+} style = {styles.button}>
+<Text style = {styles.buttonText}>Search By Author</Text>
+</TouchableOpacity>
+<TouchableOpacity onPress = {()=>{
+this.setState({
+searchWhere:'storyTitle',
+allEntries:[],
+lastVisibleEntry:null,
+isSearchPressed:true,
+})              
+this.searchByFilter('storyTitle')}
+}  style = {styles.button}>
+<Text style = {styles.buttonText}>Search By Title</Text>
+</TouchableOpacity>
+</View>
+
+<FlatList
+data = {this.state.allEntries}
+renderItem = {
+({ item })=>{
+//console.log(item);
+return(
+<View style = {styles.flatListContainer}>
+<Text style = {styles.flatListText}>{'Story Author: '+item.storyAuthor}</Text>
+<Text style = {styles.flatListText}>{'Story Title: '+item.storyTitle}</Text>
+</View>
+)
+}
+}
+keyExtractor = {
+(item,index)=>{
+return index.toString();
+}
+}
+onEndReached = {this.fetchMoreEntries}
+onEndReachedThreshold = {0.7}
+
+></FlatList>
+</KeyboardAvoidingView>
+)
+}
+}  
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20
-  },
-  searchBar: {
-    flexDirection: 'row',
-    height: 40,
-    width: 'auto',
-    borderWidth: 1,
-    alignItems: 'center',
-    padding: 10,
-  },
-  bar: {
-    borderWidth: 2,
-    height: 30,
-    width: 280,
-    paddingLeft: 10,
-    borderRadius: 30,
-  },
-  searchButton: {
-    borderWidth: 1,
-    height: 30,
-    width: 80,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 30
-  }
-});
+container:{
+flex:1,
+backgroundColor:'pink',
+},
+flatListContainer:{
+backgroundColor:'pink',
+borderWidth:5,
+borderColor:'black',
+margin:5,
+},
+flatListText:{
+color:'white',
+paddingLeft:5,
+},
+flatList:{
+backgroundColor: 'pink',
+borderTopWidth:4,
+borderColor:'pink',
+},
+button:{
+padding:5,
+marginTop:5,
+backgroundColor:'white', 
+width:115,
+height:25,
+alignSelf:'center',
+},
+buttonText:{
+color:'black',
+textAlign:'center',
+fontSize:11,
+}
+})
