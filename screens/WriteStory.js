e7117ec88360d3eb90bab97f3c72def05e8c88b5
nsinturi07 <65034759+nsinturi07@users.noreply.github.com>
2021-02-07 17:03:37 -0600

    Update WriteStory.js

--- screens/WriteStory.js
+++ screens/WriteStory.js
@@ -1,12 +1,54 @@
 import React from 'react'
-import { View,Text } from 'react-native'
+import { View,Text,StyleSheet } from 'react-native'
 
 export default class WriteScreen extends React.Component {
     render(){
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <TextInput style={styles.inputBox} placeholder="Story Title"/>
                 <Text>Write a story</Text>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'top',
+    alignItems: 'center'
+  },
+  
+  inputBox:{
+    width: 300,
+    height: 30,
+    borderWidth: 1.5,
+
+    fontSize: 20
+  },
+  inputView:{
+    flexDirection: 'row',
+    margin: 10
+  },
+  
+  textArea: {
+    height: 150,
+    justifyContent: "flex-start",
+    width : 300,
+    borderWidth: 1.5,
+  },
+  submitButton:{
+    backgroundColor: '#FBC02D',
+    width: 100,
+    height:30,
+    alignSelf: 'center',
+    marginTop: 100
+
+  },
+  submitButtonText:{
+
+    textAlign: 'center',
+    fontSize: 15,
+    fontWeight:"bold",
+    color: 'white'
+  }
+});
